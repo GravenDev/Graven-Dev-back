a0c65c7dc5515375cf4724c729248d6a6591d6b9
g_Clems <clementsol0110@gmail.com>
2022-02-02 17:16:09 +0100

    added gradle files added main added application.properties

---
+++ src/main/java/fr/gravendev/gravendevback/GravenDevBackApplication.java
@@ -0,0 +1,12 @@
+package fr.gravendev.gravendevback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GravenDevBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GravenDevBackApplication.class, args);
+    }
+}
